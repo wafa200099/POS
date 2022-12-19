a8ec2430d8c890c71c1f77ff86207bb726b0e268
wafa200099 <wafaa.alomour@gmail.com>
2022-12-19 11:18:29 +0200

    copy pos files befor migrtation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-    <App />
-
-);
-
+const root = document.getElementById("root");
+render(<App />, root);
